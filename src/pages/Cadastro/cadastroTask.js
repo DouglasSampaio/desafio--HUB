52ebc97e3ff6ad78b03140687a712fa613079cc3
smartprecast.nb4@gmail.com <douglasaraujosampaio@gmail.com>
2021-03-15 01:28:07 -0300

    teste em nuvem https://desafio-hub.herokuapp.com/

--- src/pages/Cadastro/cadastroTask.js
+++ src/pages/Cadastro/cadastroTask.js
@@ -15,7 +15,7 @@ class AddEditForm extends React.Component {
         this.setState({ [e.target.name]: e.target.value })
     }
     submitFormAdd = () => {
-        fetch(`https://desafio-hub.herokuapp.com/${localStorage.getItem('id')}`, {
+        fetch(`https://desafio-hub.herokuapp.com/task/${localStorage.getItem('id')}`, {
             method: 'post',
             headers: {
                 'Content-Type': 'application/json'
@@ -42,7 +42,7 @@ class AddEditForm extends React.Component {
     }
 
     submitFormEdit = () => {
-        fetch(`https://desafio-hub.herokuapp.com/${parseInt(this.state.id)}`, {
+        fetch(`https://desafio-hub.herokuapp.com/tasks/${parseInt(this.state.id)}`, {
             method: 'put',
             headers: {
                 'Content-Type': 'application/json'
--- src/pages/Cadastro/cadastroUsuario.js
+++ src/pages/Cadastro/cadastroUsuario.js
@@ -13,7 +13,7 @@ class AddEditForm extends React.Component {
         this.setState({ [e.target.name]: e.target.value })
     }
     submitFormAdd = () => {
-        fetch(`https://desafio-hub.herokuapp.com/`, {
+        fetch(`https://desafio-hub.herokuapp.com/users/`, {
             method: 'post',
             headers: {
                 'Content-Type': 'application/json'
