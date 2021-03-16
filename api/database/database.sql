f4d80c433349e4e406f1094c23d25473727da8c4
smartprecast.nb4@gmail.com <douglasaraujosampaio@gmail.com>
2021-03-16 00:26:30 -0300

    rename user and modification to script 'database' for postgres

--- api/database/database.sql
+++ api/database/database.sql
@@ -2,16 +2,68 @@ CREATE DATABASE users
 CREATE DATABASE tasks
 
 
+//CRIAÇÃO DB
+CREATE DATABASE "hubDev"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
 
--- module.exports={
---     dialect:'postgres',
---     host:'localhost',
---     username:'postgres',
---     password:'postgres',
---     database:'hubDev',
---     define:{
---         timestamps: true,
---         underscored: true,
---     },
+COMMENT ON DATABASE "hubDev"
+    IS 'DB';
 
--- };
+
+
+//CREATE TABLE TASKS
+    -- Table: public.tasks
+
+-- DROP TABLE public.tasks;
+
+CREATE TABLE public.tasks
+(
+    id integer NOT NULL DEFAULT nextval('tasks_id_seq'::regclass),
+    name character varying(225) COLLATE pg_catalog."default" NOT NULL,
+    description character varying(225) COLLATE pg_catalog."default" NOT NULL,
+    data character varying(12) COLLATE pg_catalog."default" NOT NULL,
+    status character varying(225) COLLATE pg_catalog."default" NOT NULL,
+    user_id integer NOT NULL,
+    CONSTRAINT tasks_pkey PRIMARY KEY (id),
+    CONSTRAINT tasks_user_id_fkey FOREIGN KEY (user_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.tasks
+    OWNER to postgres;
+
+
+
+
+//CREATE TABLE USERS
+    -- Table: public.users
+
+-- DROP TABLE public.users;
+
+CREATE TABLE public.users
+(
+    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.users
+    OWNER to postgres;
